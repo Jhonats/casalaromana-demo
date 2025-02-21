12fe7fb76709482b99c967bef40d14611a2e8ab9
Jhonats <jhonatanalfonsoferrermartinez@gmail.com>
2025-02-21 15:57:55 -0400

    First aditions to the History section

--- casalaromana-demo/src/app/components/Home/History.tsx
+++ casalaromana-demo/src/app/components/Home/History.tsx
@@ -1,31 +1,118 @@
-export default function History() {
+"use client";
+import { useRef } from "react";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const timeLineEvents = [
+  {
+    year: "2000",
+    image: "/images/History/history.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sit",
+  },
+  {
+    year: "2005",
+    image: "/images/History/history.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sit",
+  },
+  {
+    year: "2010",
+    image: "/images/History/history.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sit",
+  },
+  {
+    year: "2015",
+    image: "/images/History/history.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sit",
+  },
+  {
+    year: "2020",
+    image: "/images/History/history.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sit",
+  },
+];
+
+const HistoryCard = ({ year, image, description, index }: any) => {
   return (
-    <section id="history" className="bg-gray-50 py-16">
-      <div className="container">
-        <h2 className="mb-8 text-center text-3xl font-bold">
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: index * 0.2 }}
+      className="bg-white relative shadow-xl rounded-lg overflow-hidden transform hover:rotate-0 transition-transform duration-300 w-full h-96 flex flex-col"
+    >
+      {/* Cabecera con Título */}
+      <div className="p-4 border-b border-gray-200">
+        <h3 className="text-2xl font-bold text-gray-800 text-center">{year}</h3>
+      </div>
+      <div className="relative flex-1">
+        <Image
+          src={image || ""}
+          alt={`Evento del año ${year}`}
+          layout="fill"
+          objectFit="cover"
+          className="rounded shadow-md"
+        />
+      </div>
+      {/* Description */}
+      <div className="p-4">
+        <p className="text-gray-600 text-center">{description}</p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default function History() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start", "end start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], ["0%", "30%"]);
+
+  return (
+    <section ref={ref} id="history" className="relative py-24 overflow-hidden">
+      <motion.div style={{ y }} className="absolute inset-0 z-0 bg-black">
+        <Image
+          src="/images/History/history.jpg"
+          alt="History Background"
+          fill
+          objectFit="cover"
+          className="opacity-20"
+        />
+      </motion.div>
+      <div className="relative z-10 container mx-auto px-6 md:px-12">
+        <motion.h2
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="text-4xl font-bold text-white mb-12 text-center"
+        >
           Nuestra Historia
-        </h2>
-        <div className="grid gap-8 md:grid-cols-2">
-          <div className="relative h-[300px] overflow-hidden rounded-lg pl-4">
-            <img
-              src="/images/history/history.jpg"
-              alt="Our History"
-              className="h-full w-full object-cover"
-            />
-          </div>
-          <div className="space-y-4">
-            <p className="text-lg text-gray-600">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <p className="text-lg text-gray-600">
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit
-              anim id est laborum.
-            </p>
+        </motion.h2>
+        <div className="relative">
+          {/* Timeline line */}
+          <div className="absolute left-1/2 transform -translate-x-1/2 w-1 h-full bg-gray-300 z-0"></div>
+
+          {/* Timeline Events */}
+          <div className="relative z-10">
+            {timeLineEvents.map((event, index) => (
+              <div
+                key={event.year}
+                className={`flex items-center mb-16 ${
+                  index % 2 === 0 ? "justify-start" : "justify-end"
+                }`}
+              >
+                <div className={`w-5/12 ${index % 2 === 0 ? "pr-8" : "pl-8"}`}>
+                  <HistoryCard {...event} index={index} />
+                </div>
+                <div className="absolute left-1/2 transform -translate-x-1/2 w-4 h-4 bg-secondary rounded-full border-4 border-white"></div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
