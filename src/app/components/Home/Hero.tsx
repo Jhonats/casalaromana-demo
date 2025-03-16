80bef76ed150f328c176d6dd87e113ead7174375
Jhonatan Ferrer <jhonatanalfonsoferrermartinez@gmail.com>
2025-03-16 15:13:02 -0400

    Fix

--- src/app/components/Home/Hero.tsx
+++ src/app/components/Home/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero() {
         <video
           src="/video/HeroVideoCompressed.mp4"
           autoPlay={true}
-          className=" h-full w-full object-cover "
+          className="h-full w-full object-cover"
           loop={true}
           muted={true}
         />
